c982b9d445a0ad1b6c4a29391b5085490062281a
dreamdeveloperr <30700956+dreamdeveloperr@users.noreply.github.com>
2017-08-15 08:54:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,8 @@ console.log('Loaded!');
 var element= document.getElementById('maintext'
 );
 element.inertHTML='new value';
-var img=document.getElementById('img');
-img.onclick=function(){
-    img.style.marginLeft='100px';
+var img=document.getElementById('ajay');
+ajay.onclick=function(){
+    ajay.style.marginLeft='100px';
     
 };

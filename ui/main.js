a8c446b85c0a10e56e6c578f97860c477b073154
dreamdeveloperr <30700956+dreamdeveloperr@users.noreply.github.com>
2017-10-13 19:50:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var img=document.getElementById('ajay');
 var marginLeft;
 function moveRight(){
     
-    img.style.marginLeft='1px';
+    img.style.marginLeft='100px';
     
 }
 img.onclick=function(){

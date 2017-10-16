ffa93382617e41d2fb2a5d2609a3ef5814b1a6eb
dreamdeveloperr <30700956+dreamdeveloperr@users.noreply.github.com>
2017-10-16 13:24:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,8 @@
-var button=document.getElementById('counter');
+
 var counter = 0;
 
 button.onclick=function () {
+    var button=document.getElementById('counter');
     counter=counter+1;
     var span=document.getElementById('count');
 
